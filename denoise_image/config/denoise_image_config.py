e6e8dc0bda79afc5f704d7e5a6447ae620133435
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-09-02 15:54:19 +0500

    update config file

--- denoise_image/config/denoise_image_config.py
+++ denoise_image/config/denoise_image_config.py
@@ -5,3 +5,9 @@ BASE_PATH = 'dirty-documents'
 TRAIN_PATH = os.path.sep.join([BASE_PATH, 'train'])
 
 CLEANED_PATH = os.path.sep.join([BASE_PATH, 'clean'])
+
+FEATURES_PATH = 'features.csv'
+
+SAMPLE_PROB = 0.02
+
+MODEL_PATH = 'model.pkl'
