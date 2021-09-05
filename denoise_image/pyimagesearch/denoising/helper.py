189dfc4de43fab9bd8df3887647bb577bf525a07
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-09-05 15:18:22 +0500

    add eps value to calculate foreground

--- denoise_image/pyimagesearch/denoising/helper.py
+++ denoise_image/pyimagesearch/denoising/helper.py
@@ -12,7 +12,7 @@ def blur_and_threshold(image, eps=1e-7):
     max_val = np.max(foreground)
 
     # min-max normalization
-    foreground = (foreground - min_val) / (max_val - min_val)
+    foreground = (foreground - min_val) / (max_val - min_val + eps)
 
     return foreground
 
