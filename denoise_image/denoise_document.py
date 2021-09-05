8c89d27eadd96331da3f0ffae5f441ff2117564c
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-09-05 21:05:54 +0500

    add code to handle command-line args

--- denoise_image/denoise_document.py
+++ denoise_image/denoise_document.py
@@ -5,3 +5,8 @@ import argparse
 import pickle
 import random
 import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--testing", required=True, help="path to input dataset")
+ap.add_argument("-s", "--sample", required=True, help="sample size for testing images")
+args = vars(ap.parse_args())
