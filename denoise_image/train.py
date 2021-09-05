c3ba0236e80378c191ae804d30dbeb152c5a23bb
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-09-05 16:55:33 +0500

    add training RFR code

--- denoise_image/train.py
+++ denoise_image/train.py
@@ -1,9 +1,47 @@
 from config import denoise_image_config as config 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.matrix import mean_squared_error
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split  
 import numpy as np
 import pickle
+
+print('loading dataset...')
+
+features = []
+targets = []
+
+for row in open(config.FEATURES_PATH):
+    # split in array and convert to float
+    row = row.strip().split(',')
+    row = [float(x) for x in row]
+
+    # extract features and target
+    features.append(row[1:])
+    targets.append(row[0])
+
+    # convert to numpy array
+    features = np.array(features)
+    targets = np.array(targets)
+
+    # split data into train and test sets
+    features_train, features_test, targets_train, targets_test = train_test_split(features, targets, test_size=0.2, random_state= 44)
+
+    # tranin model
+    print('training model...')
+    model = RandomForestRegressor(n_estimators=10, random_state=44)
+    model.fit(features_train, targets_train)
+
+    # evaulate model
+    pred = model.predict(features_test)
+    mse = mean_squared_error(pred, targets_test)
+    print('mse: {}'.format(mse))
+
+    # save model
+    f = open(config.MODEL_PATH, 'wb') 
+    f.write(pickle.dumps(model))
+    f.close()
+
+
 
 
 
