463f59006fb07d837199ef989028801a302b176e
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-09-06 01:32:57 +0500

    remove redundant code

--- denoise_image/denoise_document.py
+++ denoise_image/denoise_document.py
@@ -6,17 +6,18 @@ import pickle
 import random
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-t", "--testing", required=True, help="path to input dataset")
-ap.add_argument("-s", "--sample", required=True, help="sample size for testing images")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-t", "--testing", required=True, help="path to input dataset")
+# ap.add_argument("-s", "--sample", required=True, help="sample size for testing images")
+# args = vars(ap.parse_args())
 
 # load the model
 model = pickle.loads(open(config.MODEL_PATH, "rb").read())
 
-image_paths = list(paths.list_images(args["testing"]))
-random.shuffle(image_paths)
-image_path = image_paths[:int(args["sample"])]
+# image_paths = list(paths.list_images(args["testing"]))
+# random.shuffle(image_paths)
+# image_path = image_paths[:int(args["sample"])]
+image_paths = list(paths.list_images('denoise_image/dirty-documents/test'))
 
 for image_path in image_paths:
   print('processing...{}'.format(image_path))
