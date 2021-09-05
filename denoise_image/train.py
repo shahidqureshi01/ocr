3b7097668c73291925729a04a38d59903f6af91c
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-09-05 16:05:04 +0500

    import modules

---
+++ denoise_image/train.py
@@ -0,0 +1,9 @@
+from config import denoise_image_config as config 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.matrix import mean_squared_error
+from sklearn.model_selection import train_test_split  
+import numpy as np
+import pickle
+
+
+
