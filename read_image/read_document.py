f5be2229e0eb2b54cfdcf6966fe77ee8563bd925
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-09-10 04:53:41 +0500

    add basic configuration

--- read_image/read_document.py
+++ read_image/read_document.py
@@ -2,3 +2,14 @@ from helper import OUTPUT_LAYERS
 from helper import decode_predictions
 import numpy as np
 import cv2
+
+img_path = ''
+model = ''
+height = 0
+width = 0
+conf_score = 0
+thresh = 0.5
+
+img = cv2.imread(img_path)
+(original_height, original_width) = img.shape[:2]
+
