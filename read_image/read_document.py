a607c008a09ae61144828afe40cc594b28e4cb99
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-09-13 15:03:16 +0500

    draw bounding boxes on image

--- read_image/read_document.py
+++ read_image/read_document.py
@@ -29,5 +29,23 @@ end = time.time()
 
 print('EAST took {:.6f} seconds'.format(end - start))
 
+# decode the predictions, then  apply non-maxima suppression to suppress weak, overlapping bounding boxes
 (rect, confidences) = decode_predictions(scores, geometry)
 idx = cv2.dnn.NMSBoxes(rect, confidences, conf_score, thresh)
+
+if len(idx) > 0:
+    # loop over the indexes we are keeping
+    for i in idx.flatten():
+        # extract the bounding box coordinates
+        box = cv2.boxPoints(rect[i])
+        # scale the bounding box coordinates based on the respective ratios
+        box[:,0] = box[:,0] * rW
+        box[:,1] = box[:,1] * rH
+        box= np.int0(box)
+
+        # draw the bounding box on the image
+        cv2.polylines(img, [box], True, (0,255,0), 3)
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows(1)
