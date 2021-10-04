cca8cbf00ca3cbbb6d56d172d59129d86901dc1e
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-10-04 12:47:53 +0500

    change height and width

--- read_image/read_document.py
+++ read_image/read_document.py
@@ -1,17 +1,23 @@
+from operator import index
+import re
 from helper import OUTPUT_LAYERS
 from helper import decode_predictions
+from helper import cleanup_text
 import numpy as np
 import cv2
 import time
 import pytesseract
+import sys
+import pandas as pd
+from imutils.contours import sort_contours
 
-img_path = '49.png'
+img_path = 'text.jpg'
 model = 'read_image/models/frozen_east_text_detection.pb'
-height = 320
-width = 320
+height = 640*2
+width = 640*2
 conf_score = 0.5
 thresh = 0.4
-padding = 0.1
+padding = 0.0
 sort = 'top-to-bottom'
 results = []
 img = cv2.imread(img_path)
@@ -28,6 +34,7 @@ blob = cv2.dnn.blobFromImage(img, 1.0, (height, width), (123.68, 116.78, 103.94)
 start = time.time()
 net.setInput(blob)
 (scores, geometry) = net.forward(OUTPUT_LAYERS)
+
 end = time.time()
 
 print('EAST took {:.6f} seconds'.format(end - start))
@@ -35,6 +42,7 @@ print('EAST took {:.6f} seconds'.format(end - start))
 # decode the predictions, then  apply non-maxima suppression to suppress weak, overlapping bounding boxes
 (rect, confidences) = decode_predictions(scores, geometry)
 #print('confidences is {}'.format(confidences))
+
 idx = cv2.dnn.NMSBoxesRotated(rect, confidences, conf_score, thresh)
 
 if len(idx) > 0:
@@ -65,14 +73,41 @@ if len(idx) > 0:
         # apply OCR to the padded region
         options = '--psm 7'
         text = pytesseract.image_to_string(padded_region, config=options)
-        #print('text is {}'.format(text))
         results.append((box, text))
-        
-results = sorted(results, key=lambda y: y[0][0][1])
-for (box, text) in results:
-    print('text is {}'.format(text))
-    cv2.polylines(img, [box], True, (0,255,0), 3)
-    cv2.putText(img, text, (box[0][0], box[0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
-cv2.imshow("Image", img)
-cv2.waitKey(0)
+  	
+results = sorted(results, key=lambda r:r[0][0][1])
+#results = sorted(results, key=sort_results(results))
+list = []
+for r in  results:
+	dict = {}
+	dict['x'] = r[0][0][0]
+	dict['y'] = r[0][0][1]
+	dict['results' ] = r
+	list.append(dict)
+
+df = pd.DataFrame(list, index=None)
+df['x'] = df['x'].astype(int)
+df['y'] = df['y'].astype(int)
+df.sort_values(by=['y', 'x'], ascending=[True, False], inplace=True)
+list = df.values.tolist()
+
+for l in list:
+	box = l[2][0]
+	text = l[2][1]
+	print('text is {}'.format(text))
+	cv2.polylines(img, [box], True, (0,255,0), 3)
+	cv2.putText(img, text, (box[0][0], box[0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
+
+	output = img.copy()
+	cv2.polylines(output, [box], True, (0, 255, 0), 2)
+
+	text = cleanup_text(text)
+	(x, y, w, h) = cv2.boundingRect(box)
+	cv2.putText(output, text, (x, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
+	cv2.imshow('Text Detection', output)
+	cv2.waitKey(0)
+
 cv2.destroyAllWindows(1)
+# cv2.imshow("Image", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows(1)
