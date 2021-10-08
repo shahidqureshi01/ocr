2dd0fd1cc3e0c110789e6c6f0566983b0c2244bb
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-10-08 23:35:30 +0500

    add server shut down function

--- front_end/main.py
+++ front_end/main.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 #from flask import Flask, render_template, session, copy_current_request_context
 import os
 
 template_path = os.path.abspath('./front_end')
-print(template_path)
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
 app = Flask(__name__, template_folder=template_path)
 
 @app.route('/')
 def home():
   return render_template("index.html")
+
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 # @app.route('/',methods = ['POST'])
 # def predict():
