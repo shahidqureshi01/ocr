8f31b350d9a58ccc20ca9baef3374ea7753944f8
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-10-08 23:28:33 +0500

    rename assests folder to static

--- front_end/main.py
+++ front_end/main.py
@@ -1,18 +1,18 @@
-from flask import Flask, request, jsonify, render_template, url_for
-from flask import Flask, render_template, session, copy_current_request_context
-from threading import Lock
-import sklearn
-from sklearn.ensemble import RandomForestClassifier
+from flask import Flask, render_template
+#from flask import Flask, render_template, session, copy_current_request_context
+import os
 
-app = Flask(__name__)
+template_path = os.path.abspath('./front_end')
+print(template_path)
+app = Flask(__name__, template_folder=template_path)
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+  return render_template("index.html")
 
-@app.route('/',methods = ['POST'])
-def predict():
-  return render_template('index.html', prediction_text="Newton Predicts {}".format(prediction[0]))
+# @app.route('/',methods = ['POST'])
+# def predict():
+#   return render_template('../front_end/index.html', prediction_text="Newton Predicts {}".format(prediction[0]))
 
 
 if __name__ == '__main__':
